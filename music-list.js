02b6f99d14a76a7245e5fb880b241801c47fcca6
Alan thapa <87306904+Alan-thapa98@users.noreply.github.com>
2021-07-30 10:57:27 +0545

    Add files via upload
    create dir of js , images , and songs and put the files

---
+++ music-list.js
@@ -0,0 +1,145 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6",
+  },
+{
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6",
+  },
+{
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1",
+  },
+
+{
+    name: "The Score - Unstoppable (Lyric Video)",
+    artist: "Lyric Video",
+    img: "music-6",
+    src: "The Score - Unstoppable (Lyric Video)",
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3",
+  },
+  
+ {
+    name: "[AMV] Pokemon Generations _ Superhero",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "[AMV] Pokemon Generations _ Superhero",
+  },
+ {
+    name: "Pirim Nalaune",
+    artist: "Barsha Raut, Aashish Sachin",
+    img: "music-6",
+    src: "Pirim Nalaune",
+  },
+//    {
+//     name: "MAYA BIRANI ",
+//     artist: "Melina Rai _ Najir Husen _ Aanchal Sharma",
+//     img: "music-6",
+//     src: "MAYA BIRANI ",
+//   },
+//   {
+//     name: " Born For This",
+//     artist: "Lyres",
+//     img: "music-6",
+//     src: "Born For This",
+//   },
+
+
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
